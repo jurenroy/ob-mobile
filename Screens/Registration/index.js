0e669fef03ca27e1f40266302cbf1b19bdce2d89
jurenroy <jurenroyabragn91019@gmail.com>
2023-04-14 12:57:48 +0800

    commit lang

--- Screens/Registration/index.js
+++ Screens/Registration/index.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Text, View, TextInput, Pressable, Image } from 'react-native';
+import { Text, View, TextInput, Pressable, Image} from 'react-native';
+
 import { globalStyles } from '../../Components/styles';
 import { useDispatch } from 'react-redux';
 import { setFirstName, setLastName, setEmail, setPassword, setImage } from '../../Slices/Data/DataSlice';
@@ -9,6 +10,7 @@ import { Statetus } from '../../Components/Statetus';
 import { Conv } from '../../Components/Conv';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useTogglePasswordVisibility} from '../../Components/useTogglePasswordVisibility'
+import { handleRegistration } from '../../Components/handleRegistration';
 
 export default function Registration({navigation}) {
   const dispatch = useDispatch()
@@ -17,6 +19,8 @@ export default function Registration({navigation}) {
   const [data, setData] = useState({
     firstName: '',
     lastName: '',
+    gender: '',
+    birthday: '',
     email: '',
     password: '',
     confirmPassword: '',
@@ -41,6 +45,18 @@ export default function Registration({navigation}) {
       
       <TextInput
       style={globalStyles.input} 
+      placeholder='Gender' 
+      value={data.gender}
+      onChangeText={text => {setData({...data,gender: text})}}/>
+
+      <TextInput
+      style={globalStyles.input} 
+      placeholder='Birthday' 
+      value={data.birthday}
+      onChangeText={text => {setData({...data,birthday: text})}}/>
+
+      <TextInput
+      style={globalStyles.input} 
       placeholder='Email Address' 
       value={data.email}
       onChangeText={text => {setData({...data,email: text})}}/>
@@ -63,93 +79,92 @@ export default function Registration({navigation}) {
         <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
       </Pressable>
 
-      <Pressable style={globalStyles.buttons} onPress={() => {        
-        if (data.firstName!=''&&data.lastName!=''&&data.email!=''&&data.password!=''&&data.confirmPassword!=''){
-          if (data.email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){         
-            if (data.password==data.confirmPassword){
-              if (data.password.length>8 && data.password.length>8){
-                dispatch(setFirstName(data.firstName))
-                dispatch(setLastName(data.lastName))
-                dispatch(setEmail(data.email))
-                dispatch(setPassword(data.password)) 
-                dispatch(setImage(Image.resolveAssetSource(profiled).uri)) 
-                global.state=0
-                global.dogState=0
-                global.catState=0
-                global.idnum= 
-                global.dogidnum= 
-                global.catidnum= 
-                global.chat=   
-                Petss.length=0  
-                Statetus.length=0
-                Statetus.push(0)
-                Statetus.push(0)
-                Statetus.push(0)
-                Statetus.push(0)
-                Statetus.push(0)
-                Statetus.push(0)
-                Statetus.push(0)
-                Statetus.push(0)
-                Statetus.push(0)
-                Statetus.push(0)
-                Conv.length=0
-                Conv.length=10
-                Conv[0]=[]
-                Conv[0].push("Interested?          ")
-                Conv[1]=[]
-                Conv[1].push("Interested?          ")
-                Conv[2]=[]
-                Conv[2].push("Interested?          ")
-                Conv[3]=[]
-                Conv[3].push("Interested?          ")
-                Conv[4]=[]
-                Conv[4].push("Interested?          ")
-                Conv[5]=[]
-                Conv[5].push("Interested?          ")
-                Conv[6]=[]
-                Conv[6].push("Interested?          ")
-                Conv[7]=[]
-                Conv[7].push("Interested?          ")
-                Conv[8]=[]
-                Conv[8].push("Interested?          ")
-                Conv[9]=[]
-                Conv[9].push("Interested?          ")
-
-                setData({
-                  name1: '',
-                  name2: '',
-                  username: '',
-                  email: '',
-                  password1: '',
-                  password2: '',
-                })
-                navigation.replace('Login')
-                alert("Account Registered")
+      <Pressable style={globalStyles.buttons} onPress={ () => {if (data.firstName!=''&&data.lastName!=''&&data.email!=''&&data.password!=''&&data.confirmPassword!=''){
+            if (data.email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){         
+              if (data.password==data.confirmPassword){
+                if (data.password.length>8 && data.password.length>8){
+                  dispatch(setFirstName(data.firstName))
+                  dispatch(setLastName(data.lastName))
+                  dispatch(setEmail(data.email))
+                  dispatch(setPassword(data.password)) 
+                  dispatch(setImage(Image.resolveAssetSource(profiled).uri)) 
+                  global.state=0
+                  global.dogState=0
+                  global.catState=0
+                  global.idnum= 
+                  global.dogidnum= 
+                  global.catidnum= 
+                  global.chat=   
+                  Petss.length=0  
+                  Statetus.length=0
+                  Statetus.push(0)
+                  Statetus.push(0)
+                  Statetus.push(0)
+                  Statetus.push(0)
+                  Statetus.push(0)
+                  Statetus.push(0)
+                  Statetus.push(0)
+                  Statetus.push(0)
+                  Statetus.push(0)
+                  Statetus.push(0)
+                  Conv.length=0
+                  Conv.length=10
+                  Conv[0]=[]
+                  Conv[0].push("Interested?          ")
+                  Conv[1]=[]
+                  Conv[1].push("Interested?          ")
+                  Conv[2]=[]
+                  Conv[2].push("Interested?          ")
+                  Conv[3]=[]
+                  Conv[3].push("Interested?          ")
+                  Conv[4]=[]
+                  Conv[4].push("Interested?          ")
+                  Conv[5]=[]
+                  Conv[5].push("Interested?          ")
+                  Conv[6]=[]
+                  Conv[6].push("Interested?          ")
+                  Conv[7]=[]
+                  Conv[7].push("Interested?          ")
+                  Conv[8]=[]
+                  Conv[8].push("Interested?          ")
+                  Conv[9]=[]
+                  Conv[9].push("Interested?          ")
+  
+                  setData({
+                    name1: '',
+                    name2: '',
+                    username: '',
+                    email: '',
+                    password1: '',
+                    password2: '',
+                  })
+                  navigation.replace('Login')
+                  alert("Account Registered")
+                }else{
+                  alert("Password too weak")
+                }              
               }else{
-                alert("Password too weak")
-              }              
+                alert("Password not match")
+              }
             }else{
-              alert("Password not match")
+              alert("Invalid Email")
             }
           }else{
-            alert("Invalid Email")
-          }
-        }else{
-          if (data.firstName==''&&data.lastName!=''&&data.email!=''&&data.password!=''&&data.confirmPassword!=''){
-            alert("Please Input Firstname")
-          }else if (data.firstName!=''&&data.lastName==''&&data.email!=''&&data.password!=''&&data.confirmPassword!=''){
-            alert("Please Input Lastname")
-          }else if (data.firstName!=''&&data.lastName!=''&&data.email==''&&data.password!=''&&data.confirmPassword!=''){
-            alert("Please Input Valid email")
-          }else if (data.firstName!=''&&data.lastName!=''&&data.email!=''&&data.password==''&&data.confirmPassword!=''){
-            alert("Please Input Password")
-          }else if (data.firstName!=''&&data.lastName!=''&&data.email!=''&&data.password!=''&&data.confirmPassword==''){
-            alert("Please Confirm password")
-          }else if (data.firstName==''||data.lastName==''||data.email==''||data.password==''||data.confirmPassword==''){
-              alert("Please Input Credentials")
-            }
-        }        
-      } }>
+            if (data.firstName==''&&data.lastName!=''&&data.email!=''&&data.password!=''&&data.confirmPassword!=''){
+              alert("Please Input Firstname")
+            }else if (data.firstName!=''&&data.lastName==''&&data.email!=''&&data.password!=''&&data.confirmPassword!=''){
+              alert("Please Input Lastname")
+            }else if (data.firstName!=''&&data.lastName!=''&&data.email==''&&data.password!=''&&data.confirmPassword!=''){
+              alert("Please Input Valid email")
+            }else if (data.firstName!=''&&data.lastName!=''&&data.email!=''&&data.password==''&&data.confirmPassword!=''){
+              alert("Please Input Password")
+            }else if (data.firstName!=''&&data.lastName!=''&&data.email!=''&&data.password!=''&&data.confirmPassword==''){
+              alert("Please Confirm password")
+            }else if (data.firstName==''||data.lastName==''||data.email==''||data.password==''||data.confirmPassword==''){
+                alert("Please Input Credentials")
+              }
+          } 
+          }}>
         <Text style={globalStyles.buttonsLabels}>Register</Text>
       </Pressable>
 
