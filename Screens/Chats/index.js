382a3131ae2e173d3ac9ea0c896302a965ae041e
jurenroy <jurenroyabragn91019@gmail.com>
2023-06-20 23:13:31 +0800

    last try

--- Screens/Chats/index.js
+++ Screens/Chats/index.js
@@ -1,14 +1,11 @@
 import { Image, Text, View, Pressable, ScrollView, ImageBackground} from 'react-native';
-import { useSelector, useDispatch } from "react-redux";
 import Logos from '../../Components/Logos';
 import { globalStyles } from '../../Components/styles';
 import global from '../../Components/global';
-import one from '../../assets/1.jpg'
 import { Petss } from '../../Components/Petss';
 import { Conv } from '../../Components/Conv';
 import bg from '../../assets/bg.png'
 import UnderConstructionPage from '../UnderConstructionPage';
-
 
 export default function Chats({navigation}) {
 
@@ -24,9 +21,7 @@ export default function Chats({navigation}) {
         <ScrollView>
         {Petss.map((Petss,Index) => {
             return (
-            <Pressable style={globalStyles.chatsss} onPress={() => {navigation.replace('Conversation') 
-              global.chat=Index
-            } }> 
+            <Pressable style={globalStyles.chatsss}> 
             <View style={globalStyles.chatView}>                
                 {Petss.uri && <Image source={{ uri: Petss.uri }} style={{ width: 70, height: 70, justifyContent: 'center', borderRadius: 100, marginBottom: 12,}} />}
                 <View  style={globalStyles.chatsd}>
